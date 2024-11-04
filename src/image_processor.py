71fc4663209570464f93c851171a86857a7d7a22
Franck <gutmann.franck@outlook.fr>
2024-11-04 16:18:52 +0100

    docs(ImageProcessor): add type annotations

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,23 +1,25 @@
 import datetime
 import os
+from typing import Final
+
 from PIL import Image
 
 class ImageProcessor:
 
-    output_path = "dataset"
-    padding_color = (114, 114, 114)
+    output_path: Final[str] = "dataset"
+    padding_color: Final[str] = (114, 114, 114)
 
-    def __init__(self, path):
+    def __init__(self, path: str):
         self._path = path
 
         if not os.path.exists(self.output_path):
             os.mkdir(self.output_path)  
 
     @staticmethod
-    def is_image(file):
+    def is_image(file: str) -> bool:
         return file.endswith(".jpeg") or file.endswith(".png") or file.endswith(".jpg")
 
-    def resize_image(self, image, size):
+    def resize_image(self, image: Image, size: int) -> Image:
         resized_image = Image.new("RGB", (size, size), self.padding_color)
         if image.width == image.height:
             resized_image.paste(image.resize((size, size)))
@@ -29,16 +31,16 @@ class ImageProcessor:
             resized_image.paste(image.resize((width, size)))
         return resized_image
 
-    def process_folder(self):
+    def process_folder(self) -> None:
         for file in os.listdir(self._path):
             if self.is_image(file):
                 self.process_image(file)
 
-    def save_image(self, image, file):
+    def save_image(self, image: Image, file: str) -> None:
         now = datetime.datetime.now()
         image.save(f"{self.output_path}/{now.strftime('%Y%m%d%H%M%S')}_{file}")
 
-    def process_image(self, file):
+    def process_image(self, file: str) -> None:
         image = Image.open(f"{self._path}/{file}")
         resized_image = self.resize_image(image, 640)
         self.save_image(resized_image, file)
